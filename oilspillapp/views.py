fc4b2543708a08a8e184ef00edd9aca64e390ade
szuzanna <zdrapka111@gmail.com>
2020-04-24 20:45:24 +0200

    Change response type for HTTP request (from text response to html file).

--- oilspillapp/views.py
+++ oilspillapp/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello Zuza Śmiech, if you let Mikołaj be so lazy, you will never finish this project!")
+    return render(request, 'oilspillapp/index.html')
